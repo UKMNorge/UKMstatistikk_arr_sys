517692fdc62c905cb3358bd265283b3a3c71f562
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-08-27 17:11:31 +0200

    antallArrangementerIFylke.ajax.php

---
+++ ajax/fylke/antallArrangementerIFylke.ajax.php
@@ -0,0 +1,29 @@
+<?php
+
+use UKMNorge\OAuth2\HandleAPICall;
+use UKMNorge\Geografi\Fylker;
+use UKMNorge\Statistikk\Objekter\StatistikkFylke;
+
+
+$handleCall = new HandleAPICall(['fylkeId', 'season'], [], ['GET', 'POST'], false);
+$fylkeId = $handleCall->getArgument('fylkeId');
+$season = $handleCall->getArgument('season');
+
+$fylke = null;
+try{
+    $fylke = Fylker::getById($fylkeId);
+} catch(Exception $e) {
+    $handleCall->sendErrorToClient('Kunne ikke hente fylke', 500);
+}
+
+$statFylke = null;
+try{
+    $statFylke = new StatistikkFylke($fylke, $season);
+} catch(Exception $e) {
+    $handleCall->sendErrorToClient('Kunne ikke hente statistikk for fylke', 401);
+}
+
+$retArr = [];
+$retArr['antall'] = $statFylke->antallArrangementerIFylke();
+$handleCall->sendToClient($retArr);
+
--- client/src/App.vue
+++ client/src/App.vue
@@ -96,7 +96,7 @@ export default {
             console.log('testAjax2');
             var data = {
                 action: 'UKMstatistikk_ajax',
-                controller: 'fylke/kommunerAktivitet',
+                controller: 'fylke/antallArrangementerIFylke',
                 fylkeId: 50,
                 season: 2024,
             };
