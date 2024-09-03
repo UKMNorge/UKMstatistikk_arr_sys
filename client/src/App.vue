392b5b00a575a8ae0b0830cb34b9251a67dfd6e2
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-09-03 10:21:39 +0200

    Test 2

--- client/src/App.vue
+++ client/src/App.vue
@@ -96,7 +96,7 @@ export default {
             console.log('testAjax2');
             var data = {
                 action: 'UKMstatistikk_ajax',
-                controller: 'land/getAntallInnslag',
+                controller: 'land/antallDeltakere',
                 season: 2009,
                 unike: true,
             };
