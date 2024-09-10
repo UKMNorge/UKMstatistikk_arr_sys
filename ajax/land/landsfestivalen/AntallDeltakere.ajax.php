dfe419187465bcac10d51728664c16c881b74c11
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-09-10 20:42:26 +0200

    Sjekker tilgang på landsfestivalen

--- ajax/land/landsfestivalen/AntallDeltakere.ajax.php
+++ ajax/land/landsfestivalen/AntallDeltakere.ajax.php
@@ -3,6 +3,7 @@
 use UKMNorge\Arrangement\UKMFestival;
 use UKMNorge\OAuth2\HandleAPICall;
 use UKMNorge\Statistikk\Objekter\StatistikkArrangement;
+use UKMNorge\Statistikk\StatistikkManager;
 
 
 $handleCall = new HandleAPICall(['season', 'unike'], [], ['GET', 'POST'], false);
@@ -18,6 +19,13 @@ try{
     }
     $handleCall->sendErrorToClient('Kunne ikke hente arrangementet', 401);
 }
+
+// IMPORTANT!!!
+// This is a security check to make sure the user has access to the arrangement
+if(StatistikkManager::hasAccessToArrangement($arrangement->getId()) != true) {
+    $handleCall->sendErrorToClient('Ingen tilgang til arrangementet', 401);
+}
+
 
 $statArr = new StatistikkArrangement($arrangement->getId(), $arrangement->getSesong());
 
