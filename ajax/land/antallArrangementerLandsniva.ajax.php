6019fea4ac1aeea9eed442743932569aa1d27ee1
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-09-13 14:44:24 +0200

    land/antallArrangementerLandsniva kun superadmin tilgang

--- ajax/land/antallArrangementerLandsniva.ajax.php
+++ ajax/land/antallArrangementerLandsniva.ajax.php
@@ -1,9 +1,13 @@
 <?php
 
-use UKMNorge\OAuth2\HandleAPICall;
 use UKMNorge\Statistikk\Objekter\StatistikkArrangement;
+use UKMNorge\Statistikk\StatistikkHandleAPICall;
 
-$handleCall = new HandleAPICall([], [], ['POST'], false);
+
+$tilgang = 'superadmin'; // Kreves tilgang som superadmin for å se statistikk for alle kommuner
+$tilgangAttribute = null; 
+
+$handleCall = new StatistikkHandleAPICall([], [], ['POST'], false, false, $tilgang, $tilgangAttribute);
 
 
 $handleCall->sendToClient(['antall' => StatistikkArrangement::getAntallArrangementer()]);
