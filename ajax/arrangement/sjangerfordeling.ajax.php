f9415d9cea7cc8accae6c074d245491ada4d6516
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-09-17 13:22:14 +0200

    Sjangerfordeling tilgangskontroll

--- ajax/arrangement/sjangerfordeling.ajax.php
+++ ajax/arrangement/sjangerfordeling.ajax.php
@@ -1,14 +1,21 @@
 <?php
 
 use UKMNorge\Arrangement\Arrangement;
-use UKMNorge\OAuth2\HandleAPICall;
-use UKMNorge\Database\SQL\Query;
+use UKMNorge\Statistikk\StatistikkHandleAPICall;
 use UKMNorge\Statistikk\Objekter\StatistikkArrangement;
 
+$plId = StatistikkHandleAPICall::getArgumentBeforeInit('plId', 'POST');
+
+if($plId == null) {
+    StatistikkHandleAPICall::sendError('Mangler arrangement ID', 400);
+}
+
+$tilgang = 'arrangement_i_kommune_fylke'; // Er admin i arrangement eller er admin i kommune eller fylke som arrangementet tilhører
+$tilgangAttribute = $plId; // Sender riktig arrangement id til tilgangskontroll
 
 
-// Det brukes POST fordi WP tillater POST bare
-$handleCall = new HandleAPICall(['plId'], [], ['GET', 'POST'], false);
+$handleCall = new StatistikkHandleAPICall(['plId'], [], ['GET', 'POST'], false, false, $tilgang, $tilgangAttribute);
+
 $plId = $handleCall->getArgument('plId');
 
 $arrangement = null;
