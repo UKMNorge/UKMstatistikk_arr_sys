d646ab4310b9d32e0711e25b527e2b7c35494fb5
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-10-11 15:06:18 +0200

    Kjonnsfordeling phantom loading

--- client/src/components/Kommune/Kjonnsfordeling.vue
+++ client/src/components/Kommune/Kjonnsfordeling.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-if="dataFetched == true && startYear == selectedYears[0] && endYear == selectedYears[selectedYears.length-1]">
-            <div class="as-margin-bottom-space-2 as-margin-top-space-2">
+            <div class="as-margin-bottom-space-4 as-margin-top-space-2">
                 <h4>
                     <template v-if="selectedYears.length > 1">
                         {{ selectedKommune.title }} kjønnsfordeling fra {{ selectedYears[0] }} til {{ selectedYears[selectedYears.length-1] }}
@@ -16,6 +16,19 @@
                 :dataset="getDataset()"
             />
         </div>
+        <div v-else-if="fetchingStarted">
+            <div class="as-margin-bottom-space-4 as-margin-top-space-2">
+                <h4>
+                    <template v-if="selectedYears.length > 1">
+                        {{ selectedKommune.title }} kjønnsfordeling fra {{ selectedYears[0] }} til {{ selectedYears[selectedYears.length-1] }}
+                    </template>
+                    <template v-else>
+                        {{ selectedKommune.title }} kjønnsfordeling for {{ selectedYears[0] }}
+                    </template>
+                </h4>
+            </div>
+            <LoadingComponent />
+        </div>
     </div>
 </template>
 
@@ -23,6 +36,7 @@
 import Pie from '../charts/Pie.vue';
 import type Kommune from '../../objects/Kommune'; // Ensure Kommune is imported correctly
 import type { PropType } from 'vue';  // Use type-only import for PropType
+import LoadingComponent from '../Other/LoadingComponent.vue';
 
 
 export default {
@@ -41,6 +55,7 @@ export default {
     },
     components: {
         Pie : Pie,
+        LoadingComponent : LoadingComponent,
     },
     data() {
         return {
@@ -49,15 +64,18 @@ export default {
             dataFetched: false,
             startYear: 0,
             endYear: 0,
+            fetchingStarted: false,
         }
     },
     methods: {
         async init() {
+            
             this.startYear = this.selectedYears[0];
             this.endYear = this.selectedYears[this.selectedYears.length-1];
-
-            // Empty old data
+            
+            this.fetchingStarted = true;
             this.dataFetched = false;
+
             this.kommuneData = [];
             this.kommuneData['female'] = 0;
             this.kommuneData['male'] = 0;
@@ -78,6 +96,7 @@ export default {
                 this.kommuneData['unknown'] += results['unknown'] ? results['unknown'] : 0;
             }
 
+            this.fetchingStarted = false;
             this.dataFetched = true;
 
         },
