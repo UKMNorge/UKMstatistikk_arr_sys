0effa7ad3c9ff282f7f77202db393d3e0d826d5b
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2024-10-21 22:26:12 +0200

    Aldersfordeling integrasjon mot arr.sys aldersfordeling

--- client/src/components/Kommune/AldersfordelingSSB.vue
+++ client/src/components/Kommune/AldersfordelingSSB.vue
@@ -10,6 +10,26 @@
                 :dataset="getDataset()"
                 :lineDataset="getLineDataset()"
             />
+            <div class="as-margin-top-space-4">
+                <PermanentNotification :typeNotification="'primary'" tittel="Info om statistikken" :isHTML="true" description="
+                <br>
+                <p>
+                    <strong>Denne statistikken viser en sammenligning mellom aldersfordelingen i kommunen basert på SSB-data og prosentandelen av deltakere i arrangørsystemet for samme aldersgrupper. Diagrammet dekker ungdommer i alderen 10 til 21 år, fordelt på følgende aldersgrupper: 10-11, 12-13, 14-15, 16-17, 18-19, og 20-21.</strong>
+                </p>
+                <br>
+                <p>
+                    - Horisontale linjene viser prosentandelen av befolkningen i kommunen i hver aldersgruppe, som er hentet fra SSB. <strong>Merk at summen av prosentandelene for alle aldersgruppene utgjør 100% av befolkningen i kommunen.</strong>. For eksempel kan vi se hvor stor andel av befolkningen som er 10-11 år, 12-13 år, osv.
+                </p>
+                <p>
+                    - Vertikale stolpene representerer prosentandelen av deltakere i arrangørsystemet innenfor hver aldersgruppe. Hvis en stolpe for aldersgruppen 18-19 år viser 40 %, betyr det at 40 % av deltakere i din kommune er mellom 18 og 19 år i en sesong.
+                </p>
+                <br>
+                <p>
+                    Ved å sammenligne disse to datasettene kan du få innsikt i hvilke aldersgrupper som er mest og minst representert i arrangementene i forhold til aldersfordelingen i befolkningen. Dette kan hjelpe med å identifisere hvilke grupper er mer engasjert og hvilke grupper kan eventuelt engasjeres.
+                </p>
+                " />
+
+            </div>
         </div>
         <div v-else-if="fetchingStarted">
             <LoadingComponent />
@@ -22,7 +42,8 @@ import MultiBarChart from '../charts/MultiBarChart.vue';
 import type Kommune from '../../objects/Kommune'; // Ensure Kommune is imported correctly
 import type { PropType } from 'vue';  // Use type-only import for PropType
 import LoadingComponent from '../Other/LoadingComponent.vue';
-import { getRandomColor } from '../../utils/Colors';  
+import { getRandomColor } from '../../utils/Colors';
+import { PermanentNotification } from 'ukm-components-vue3';
   
 export default {
     props: {
@@ -40,6 +61,7 @@ export default {
     },
     components: {
         LoadingComponent,
+        PermanentNotification,
         MultiBarChart : MultiBarChart,
     },
     data() {
@@ -102,8 +124,8 @@ export default {
         },
         getLineDataset() : any {
             let retArr: { [key: string]: any } = [];
-            // console.log(1725);
-            // console.log(this.arrSysAldersfordeling);
+            let totalPopulationYear = {} as any;
+
             for(let year of this.selectedYears) {
                 retArr[year.toString()] = [];
                 retArr[year.toString()]['10-11'] = 0;
@@ -114,12 +136,12 @@ export default {
                 retArr[year.toString()]['20-21'] = 0;
 
                 for(let aldersFordel of this.arrSysAldersfordeling[year]) {
-                    console.log(1723)
-                    console.log(aldersFordel);
 
                     if(aldersFordel.age < 10 || aldersFordel.age > 21) {
                         continue;
                     }
+
+                    totalPopulationYear[year] = totalPopulationYear[year] ? totalPopulationYear[year] + parseInt(aldersFordel.antall) : parseInt(aldersFordel.antall);
 
                     let idAlder = aldersFordel.age % 2 !== 0 ? (parseInt(aldersFordel.age) - 1) + '-' + aldersFordel.age : aldersFordel.age + '-' + (parseInt(aldersFordel.age) + 1);
 
@@ -133,9 +155,9 @@ export default {
             for(let year of this.selectedYears) {
                 let data = [];
                 for(let key in retArr[year]) {
-                    data.push(retArr[year][key]);
+                    data.push(retArr[year][key] / totalPopulationYear[year] * 100);
                 }
-                let color = getRandomColor(1, colorId);
+                let color = getRandomColor(.5, colorId);
                 lineDataset.push({
                     label: this.selectedKommune.title + ' ' + year.toString(),
                     data: data, 
@@ -151,7 +173,7 @@ export default {
         getDataset() : any {
 
             var dataArr = [] as any;
-            let totalPopulationYear = [] as any;
+            let totalPopulationYear = {} as any;
             
             for(let kData in this.kommunerData) {                
                 for(let d of this.kommunerData[kData]) {
@@ -165,10 +187,12 @@ export default {
                     dataArr['' + year]['18-19'] = 0;
                     dataArr['' + year]['20-21'] = 0;
 
-                    totalPopulationYear['' + year] = d.data.total_population;
+                    totalPopulationYear['' + year] = 0;
 
                     for(let key in d.data.age_distribution) {
                         let alder = d.data.age_distribution[key];
+
+                        totalPopulationYear['' + year] += parseInt(alder.antall);
 
                         var idAlder = '';
 
@@ -182,21 +206,20 @@ export default {
             var retArr = [] as any;
             let colorId = 0;
             for(let year in dataArr) {
-                console.warn('totalPopulationYear');
-                console.warn(totalPopulationYear[year]);
                 let kData = dataArr[year];
-                
+                let totalPopulation = totalPopulationYear[year];
 
                 // Calculate percentage. Divide by the ages by 2 because we have two years in each age group 
                 for(let kDataKey in dataArr[year]) {
-                    kData[kDataKey] = kData[kDataKey];
+                    kData[kDataKey] = (kData[kDataKey] / totalPopulation) * 100;
+
                 }
-                console.log(dataArr[year]);
+                
                 let opacityColor = 1; //kData.kommune.id == 0 ? 1 : .4;
                 let color = getRandomColor(opacityColor, colorId);
                 retArr.push(
                     {
-                        label: 'SSB ' + year,
+                        label: this.selectedKommune.title + ' SSB ' + year,
                         type: 'line',
                         tension: 0.2, // Smoothen the line
                         borderColor: color,
