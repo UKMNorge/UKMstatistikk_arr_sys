c92b6074cf7aed30d5fa8d420495e6aeed2ffd10
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-02-13 17:48:10 +0100

    Antalldeltakere på nasjonalt oppdatering

--- ajax/land/antallDeltakere.ajax.php
+++ ajax/land/antallDeltakere.ajax.php
@@ -3,30 +3,19 @@
 use UKMNorge\DesignWordpress\Environment\Statistikk;
 use UKMNorge\Geografi\Fylke;
 use UKMNorge\OAuth2\ArrSys\HandleAPICallWithAuthorization;
-use UKMNorge\Statistikk\Objekter\StatistikkFylke;
+use UKMNorge\Statistikk\Objekter\StatistikkNasjonalt;
 
 
-$tilgang = 'fylke';
-$tilgangAttribute = null; // Er admin i minst 1 fylke
+$tilgang = 'superadmin'; // Kreves tilgang som superadmin for å se statistikken
+$tilgangAttribute = null;
 
 $handleCall = new HandleAPICallWithAuthorization(['season', 'unike'], [], ['GET', 'POST'], false, false, $tilgang, $tilgangAttribute);
 
 $season = $handleCall->getArgument('season');
 $erUnike = $handleCall->getArgument('unike') == 'true';
 
-$alleFylkerISesong = StatistikkFylke::getAlleFylkeIdFraSSB($season);
+$statNasjonalt = new StatistikkNasjonalt($season);
 
-$antall = 0;
-foreach($alleFylkerISesong as $fylkeId => $fylkeNavn) {
-    $fylke = new Fylke($fylkeId, '', (string)$fylkeNavn, true);
-    $statistikkFylke = new StatistikkFylke($fylke, $season);
+$antall = $erUnike ? $statNasjonalt->getAntallUnikeDeltakere() : $statNasjonalt->getAntallDeltakere();
 
-    if($erUnike) {
-        $antallDeltakere = $statistikkFylke->getAntallUnikeDeltakere();
-    } else {
-        $antallDeltakere = $statistikkFylke->getAntallDeltakere();
-    }
-    $antall += $antallDeltakere;
-}
-
-$handleCall->sendToClient(['antall' => $antall]);
+$handleCall->sendToClient(['unike' => $erUnike, 'antall' => $antall]);
