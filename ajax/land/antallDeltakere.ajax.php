6300b6a2a9c2f54796975f658043220cb6000ca2
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-02-14 13:14:40 +0100

    Nasjonalt mappe med antallDeltakere

--- ajax/land/antallDeltakere.ajax.php
+++ ajax/land/antallDeltakere.ajax.php
@@ -3,19 +3,32 @@
 use UKMNorge\DesignWordpress\Environment\Statistikk;
 use UKMNorge\Geografi\Fylke;
 use UKMNorge\OAuth2\ArrSys\HandleAPICallWithAuthorization;
-use UKMNorge\Statistikk\Objekter\StatistikkNasjonalt;
+use UKMNorge\Statistikk\Objekter\StatistikkFylke;
 
 
-$tilgang = 'superadmin'; // Kreves tilgang som superadmin for å se statistikken
-$tilgangAttribute = null;
+$tilgang = 'fylke';
+$tilgangAttribute = null; // Er admin i minst 1 fylke
 
 $handleCall = new HandleAPICallWithAuthorization(['season', 'unike'], [], ['GET', 'POST'], false, false, $tilgang, $tilgangAttribute);
 
 $season = $handleCall->getArgument('season');
 $erUnike = $handleCall->getArgument('unike') == 'true';
 
-$statNasjonalt = new StatistikkNasjonalt($season);
+$alleFylkerISesong = StatistikkFylke::getAlleFylkeIdFraSSB($season);
 
-$antall = $erUnike ? $statNasjonalt->getAntallUnikeDeltakere() : $statNasjonalt->getAntallDeltakere();
+$antall = 0;
+foreach($alleFylkerISesong as $fylkeId => $fylkeNavn) {
+    $fylke = new Fylke($fylkeId, '', (string)$fylkeNavn, true);
+    $statistikkFylke = new StatistikkFylke($fylke, $season);
+    
+    var_dump($fylke->getNavn() . ' = ' . $statistikkFylke->getAntallUnikeDeltakere() .' |');
 
-$handleCall->sendToClient(['unike' => $erUnike, 'antall' => $antall]);
+    if($erUnike) {
+        $antallDeltakere = $statistikkFylke->getAntallUnikeDeltakere();
+    } else {
+        $antallDeltakere = $statistikkFylke->getAntallDeltakere();
+    }
+    $antall += $antallDeltakere;
+}
+
+$handleCall->sendToClient(['antall' => $antall]);
---
+++ ajax/nasjonalt/antallDeltakere.ajax.php
@@ -0,0 +1,21 @@
+<?php
+
+use UKMNorge\DesignWordpress\Environment\Statistikk;
+use UKMNorge\Geografi\Fylke;
+use UKMNorge\OAuth2\ArrSys\HandleAPICallWithAuthorization;
+use UKMNorge\Statistikk\Objekter\StatistikkNasjonalt;
+
+
+$tilgang = 'superadmin'; // Kreves tilgang som superadmin for å se statistikken
+$tilgangAttribute = null;
+
+$handleCall = new HandleAPICallWithAuthorization(['season', 'unike'], [], ['GET', 'POST'], false, false, $tilgang, $tilgangAttribute);
+
+$season = $handleCall->getArgument('season');
+$erUnike = $handleCall->getArgument('unike') == 'true';
+
+$statNasjonalt = new StatistikkNasjonalt($season);
+
+$antall = $erUnike ? $statNasjonalt->getAntallUnikeDeltakere() : $statNasjonalt->getAntallDeltakere();
+
+$handleCall->sendToClient(['unike' => $erUnike, 'antall' => $antall]);
