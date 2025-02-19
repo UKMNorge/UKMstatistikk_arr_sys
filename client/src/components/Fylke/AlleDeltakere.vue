c1dc588dca943acb647d6bf0070c6d6677670932
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-02-19 15:51:58 +0100

    Antall deltakere på flere fylker

--- client/src/components/Fylke/AlleDeltakere.vue
+++ client/src/components/Fylke/AlleDeltakere.vue
@@ -5,27 +5,26 @@
             <MultiBarChart ref="chart"
                 :labels="getLabels()" 
                 :dataset="getDataset()"
+                :labelCallbackFunction="(tooltipItem) => `${tooltipItem.raw} deltaker${tooltipItem.raw > 1 ? 'e' : ''}`"
+                :titleCallbackFunction="titleCallbackFunction"
             />
         </div>
-
         <div v-else-if="fetchingStarted">
             <LoadingComponent />
         </div>
+        
     </div>
 </template>
 
 <script lang="ts">
 import MultiBarChart from '../charts/MultiBarChart.vue';
 import LoadingComponent from '../Other/LoadingComponent.vue';
-import type { PropType } from 'vue';  // Use type-only import for PropType
-import type Fylke from '../../objects/Fylke';
-import { getRandomColor } from '../../utils/Colors';
-
+import { getRandomColor } from '../../utils/Colors';  
 
 export default {
     props: {
-        selectedFylkeId: {
-            type: Object as any,
+        selectedFylker: {
+            type: Array as () => number[],
             required: true
         },
         selectedYears: {
@@ -39,15 +38,15 @@ export default {
     components: {
         MultiBarChart : MultiBarChart,
         LoadingComponent : LoadingComponent,
-
     },
     data() {
         return {
             spaInteraction : (<any>window).spaInteraction, // Definert i main.ts
-            fylkeData: {} as any, //{kommune : Kommune, year : number, antall : number}[]
+            fylkerData: {} as any,
             dataFetched: false,
             colors : ['#FF6384', '#36A2EB', '#FFCE56'],
             fetchingStarted: false,
+            alleFylker: {} as any,
         }
     },
     methods: {
@@ -55,31 +54,43 @@ export default {
             // Empty old data
             this.fetchingStarted = true;
             this.dataFetched = false;
-            this.fylkeData = [];
+            this.fylkerData = {};
 
-            for(let year of this.selectedYears) {
-                var data = {
-                    action: 'UKMstatistikk_ajax',
-                    controller: 'fylke/antallDeltakere',
-                    fylkeId: this.selectedFylkeId,
-                    season: year,
-                    unike: true
-                };
+            for(let fylke of this.selectedFylker) {
+                for(let year of this.selectedYears) {
+                    var data = {
+                        action: 'UKMstatistikk_ajax',
+                        controller: 'fylke/antallDeltakere',
+                        fylkeId: fylke,
+                        season: year,
+                        unike: true
+                    };
 
-                var results = await this.spaInteraction.runAjaxCall('/', 'POST', data);
+                    var results = await this.spaInteraction.runAjaxCall('/', 'POST', data);
 
-                var arr = {
-                    fylkeNavn: results.fylkeNavn,
-                    year: year,
-                    antall: results.antall
+                    if (!this.alleFylker[fylke]) {
+                        this.alleFylker[fylke] = {};
+                    }
+                    if (!this.alleFylker[fylke][year]) {
+                        this.alleFylker[fylke][year] = {};
+                    }
+
+                    var arr = {
+                        fylke: fylke,
+                        year: year,
+                        antall: results.antall
+                    }
+
+                    if(this.fylkerData[year] == undefined) {
+                        this.fylkerData[year] = [];
+                    }
+
+                    this.fylkerData[year].push(arr);
                 }
-
-                if(this.fylkeData[year] == undefined) {
-                    this.fylkeData[year] = [];
-                }
-
-                this.fylkeData[year].push(arr);
             }
+
+            console.log(1246);
+            console.log(this.alleFylker);
 
             this.fetchingStarted = false;
             this.dataFetched = true;
@@ -87,28 +98,79 @@ export default {
         },
         getLabels() : any {
             let retArr = [];
-            
-            for(let year of this.selectedYears) {
-                retArr.push(year.toString());
+
+            if(this.selectedFylker.length == 1) {
+                for(let year of this.getAllSelectedYears()) {
+                    retArr.push(year.toString());
+                }
+            } else {
+                let counter = 0;
+                for(let fylke of this.selectedFylker) {
+                    retArr.push(this._getFylkeById(parseInt((<any>fylke))));
+                    counter++;
+                }
             }
 
             return retArr;
         },
+        _getFylkeById(fylkeId : number) : string {
+            let omrader = (<any>window).ukm_statistikk_klient.omrade
+            for(let omrade of omrader) {
+                if(omrade.type == 'fylke' && omrade.id == fylkeId) {
+                    return omrade.name;
+                }
+            }
+            return 'Ukjent';
+        },
         getDataset() : any { 
             var retArr = [] as any;
-            
-            let fylkeAntallData = [];
-            let fylkeNavn = 'Unike deltakere fra kommuner i ' + this.fylkeData[this.selectedYears[0]][0].fylkeNavn;
+            var singleRetArr = [] as any;
+                        
+            var count = 0;
+            let colorId = 0;
             for(let year of this.selectedYears) {
-                fylkeAntallData.push(this.fylkeData[year][0].antall);
+                var dataKomm = [];
+                for(let data of this.fylkerData[year]) {
+                    dataKomm.push(data.antall);
+                    singleRetArr.push(data.antall);
+
+                }
+                
+                if(this.selectedFylker.length > 1) {
+                    retArr.push({
+                        label: year.toString(),
+                        data: dataKomm, 
+                        backgroundColor: getRandomColor(1, colorId),
+                    });
+                }
+                count++;
+                colorId++;
             }
-            retArr.push({
-                label: fylkeNavn,
-                data: fylkeAntallData, 
-                backgroundColor: getRandomColor(1, 0),
-            });
+
+            console.log(retArr);
+
+            if(this.selectedFylker.length == 1) {
+                retArr.push({
+                    label: 'Antall deltakere i ' + this._getFylkeById(this.selectedFylker[0]),
+                    data: singleRetArr, 
+                    backgroundColor: getRandomColor(1, 0),
+                });
+            }
 
             return retArr;
+        },
+        getAllSelectedYears(): number[] {
+            var firstYear = this.selectedYears[0];
+            var lastYear = this.selectedYears[this.selectedYears.length - 1];
+            var years = [];
+            for (let i = firstYear; i <= lastYear; i++) {
+                years.push(i);
+            }
+
+            return years;
+        },
+        titleCallbackFunction(tooltipItem : any) {
+            return tooltipItem[0].dataset.label;
         }
     }
 }
@@ -118,3 +180,5 @@ export default {
 <style scoped>
 
 </style>
+
+
