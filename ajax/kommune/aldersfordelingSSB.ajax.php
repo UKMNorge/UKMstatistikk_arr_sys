15361a6381194c05a4e3bdc094dac0208f9f0cc0
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-02-20 21:48:55 +0100

    getAldersfordelingKommune returnerer ikke data for nåværende år

--- ajax/kommune/aldersfordelingSSB.ajax.php
+++ ajax/kommune/aldersfordelingSSB.ajax.php
@@ -8,6 +8,10 @@ $handleCall = new HandleAPICall(['kommuneId', 'year'], [], ['GET', 'POST'], fals
 $kommuneId = $handleCall->getArgument('kommuneId');
 $year = $handleCall->getArgument('year');
 
-
-$handleCall->sendToClient(StatistikkSSB::getAldersfordelingKommune($kommuneId, $year));
-
+if ($year < date('Y')) {
+    $handleCall->sendToClient(StatistikkSSB::getAldersfordelingKommune($kommuneId, $year));
+} else {
+    $handleCall->sendToClient([
+        'age_distribution' => []
+    ]);
+}
