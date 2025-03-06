d6503a0a59c36da9e8585e8d31f5b1d06f24c469
Kushtrim Aliu <kushtrimaliu19@gmail.com>
2025-03-06 16:38:59 +0100

    Arrangement TS klasse

---
+++ client/src/objects/Arrangement.ts
@@ -0,0 +1,11 @@
+class Arrangement {
+    id: number;
+    title: string
+
+    constructor(id : number, title : string) {
+        this.id = id;
+        this.title = title;
+    }
+}
+
+export default Arrangement;
